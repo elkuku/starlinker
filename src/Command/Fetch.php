4382fe6ed6609945db3030c61cac21bf9ccd7525
Nikolai Plath <der.el.kuku@gmail.com>
2017-12-12 11:57:24 -0500

    Update fetch command

--- src/Command/Fetch.php
+++ src/Command/Fetch.php
@@ -9,10 +9,12 @@
 namespace App\Command;
 
 
+use App\Helper\StarLinker;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
 
 class Fetch extends ContainerAwareCommand
 {
@@ -26,7 +28,9 @@ class Fetch extends ContainerAwareCommand
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$root = $this->getContainer()->get('kernel')->getProjectDir();
+		$io = new SymfonyStyle($input, $output);
+
+		$root     = $this->getContainer()->get('kernel')->getProjectDir();
 		$fileName = $root . '/data/' . date('Y-m-d--H') . '-starlink-data.txt';
 
 		$data = $this->fetchData();
@@ -34,6 +38,28 @@ class Fetch extends ContainerAwareCommand
 		file_put_contents($fileName, $data);
 
 		$output->writeln(sprintf('File has been saved to "%s"', $fileName));
+
+		$lines = StarLinker::sortByField(
+			explode("\n", trim($data)), 3);
+
+		$topten = [];
+
+		for ($i = 0; $i < 5; $i++)
+		{
+			$d = explode("\t", $lines[$i]);
+
+			$topten[$d[1]] = $d[3];
+		}
+
+		$response          = new \stdClass();
+		$response->time    = (new \DateTime())->format('H:i');
+		$response->topten   = $topten;
+
+		$outputFile = $root.'/data/'.(new \DateTime())->format('Y-m-d').'-starlink-data.txt';
+
+		$io->text('write to ' . $outputFile);
+		$fs = new Filesystem();
+		$fs->appendToFile($outputFile, json_encode($response) . "\n");
 	}
 
 	private function fetchData()
