5b911de47cbd5c22348eec206a510f0fa1feb262
Nikolai Plath <der.el.kuku@gmail.com>
2017-12-12 09:48:09 -0500

    Update fetch command

--- src/Command/Fetch.php
+++ src/Command/Fetch.php
@@ -9,12 +9,12 @@
 namespace App\Command;
 
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Fetch extends Command
+class Fetch extends ContainerAwareCommand
 {
 	protected function configure()
 	{
@@ -26,30 +26,22 @@ class Fetch extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$output->writeln('Whoa!');
+		$root = $this->getContainer()->get('kernel')->getProjectDir();
+		$fileName = $root . '/data/' . date('Y-m-d--H') . '-starlink-data.txt';
 
 		$data = $this->fetchData();
 
-		$table = new Table($output);
-		$table
-			->setHeaders(array('#', 'Captain', 'Score'));
+		file_put_contents($fileName, $data);
 
-		for ($i = 0; $i < 5; $i++)
-		{
-			$d = explode("\t", $data[$i]);
-			$table->addRow([$i + 1, $d[1], $d[3]]);
-		}
-
-		$table->render();
+		$output->writeln(sprintf('File has been saved to "%s"', $fileName));
 	}
 
 	private function fetchData()
 	{
-
 		$url = 'http://starlink.tasharen.com/ranked.php';
 
-		$ch=curl_init();
-		$timeout=5;
+		$ch      = curl_init();
+		$timeout = 5;
 
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -59,23 +51,6 @@ class Fetch extends Command
 
 		curl_close($ch);
 
-		$lines = explode("\n", $content);
-		array_pop($lines);
-
-		usort($lines, function ($a, $b){
-			$as = explode("\t", $a);
-			$bs = explode("\t", $b);
-
-			$sField = 3;
-
-			if ($as[$sField] === $bs[$sField])
-			{
-				return 0;
-			}
-
-			return $as[$sField] > $bs[$sField] ? -1 : 1;
-		});
-
-		return $lines;
+		return $content;
 	}
 }
