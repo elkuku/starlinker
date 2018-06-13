79589057687742a36be4a6cf1b97b275ee809f54
Nikolai Plath <der.el.kuku@gmail.com>
2018-06-13 09:46:45 -0500

    Add util classes

---
+++ src/Util/Chart.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 13.06.18
+ * Time: 09:07
+ */
+
+namespace App\Util;
+
+class Chart
+{
+	public $header = '';
+	public $data = '';
+}
---
+++ src/Util/UserDiff.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 13.06.18
+ * Time: 09:41
+ */
+
+namespace App\Util;
+
+class UserDiff
+{
+	public $start = 0;
+	public $end = 0;
+	public $diff = 0;
+}
