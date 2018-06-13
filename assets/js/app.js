12ba6735e5cbf18cb320c5de5aff87094c5b3f70
Nikolai Plath <der.el.kuku@gmail.com>
2018-06-13 09:46:17 -0500

    Remove datepicker ref

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,3 @@ require('../../node_modules/bootstrap/dist/js/bootstrap');
 require('../css/app.css');
 
 require('chart.js');
-
-$(document).ready(function() {
-    $('.js-datepicker').datepicker();
-});
-
